4ff4e2af2bb28267f88855a6f23bcf5e81fdc338
Chou KuanYu <chousml@gmail.com>
2017-03-22 08:27:48 +0800

    Add project files.

---
+++ EmguReadVideo/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace EmguReadVideo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnQueueClear = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnJump = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(535, 12);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(101, 37);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "讀影片";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnQueueClear
+            // 
+            this.btnQueueClear.Location = new System.Drawing.Point(535, 55);
+            this.btnQueueClear.Name = "btnQueueClear";
+            this.btnQueueClear.Size = new System.Drawing.Size(101, 37);
+            this.btnQueueClear.TabIndex = 1;
+            this.btnQueueClear.Text = "清佇列";
+            this.btnQueueClear.UseVisualStyleBackColor = true;
+            this.btnQueueClear.Click += new System.EventHandler(this.btnQueueClear_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(535, 98);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(101, 37);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "播放";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(535, 141);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(101, 37);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.Text = "暫停";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(516, 332);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 366);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "存在佇列中的影格索引:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(253, 366);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "_";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(404, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "FrameRate:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(512, 366);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "_";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(536, 231);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(536, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "輸入指定影格";
+            // 
+            // btnJump
+            // 
+            this.btnJump.Location = new System.Drawing.Point(535, 259);
+            this.btnJump.Name = "btnJump";
+            this.btnJump.Size = new System.Drawing.Size(101, 37);
+            this.btnJump.TabIndex = 11;
+            this.btnJump.Text = "跳至該影格";
+            this.btnJump.UseVisualStyleBackColor = true;
+            this.btnJump.Click += new System.EventHandler(this.btnJump_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 405);
+            this.Controls.Add(this.btnJump);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnQueueClear);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnQueueClear;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnJump;
+    }
+}
+
---
+++ EmguReadVideo/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace EmguReadVideo
+{
+    public partial class Form1 : Form
+    {
+        //Emgucv3.1 官方字典使用說明書Link:
+        // http://www.emgu.com/wiki/index.php/Version_History#Emgu.CV-3.1.0
+        // http://www.emgu.com/wiki/files/3.1.0/document/html/8dee1f02-8c8a-4e37-87f4-05e10c39f27d.htm
+
+        # region variable 
+        Mat currentFrame;
+        Capture grabber;
+        int videoFps = 0;//frame rate
+        int frameNumber=0;
+        bool isJump = false;
+        #endregion
+
+        //C# Queue 官方使用 link:
+        //https://msdn.microsoft.com/en-us/library/7977ey2c(v=vs.110).aspx
+        //
+        //Stackoverflow上有人曾經將Queue跟emgucv結合
+        //http://stackoverflow.com/questions/11612046/parallel-image-processing-artifacts
+        //
+
+        #region datastructure variable
+        Queue<Mat> inQueue = new Queue<Mat>(); //物件尚未實體化就呼叫它的屬性或方法  記得先實體化
+        //inQueue 是 global variable 
+        #endregion
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "3gpp文件|*.3gpp|MP4文件|*.mp4|AVI文件|*.avi|RMVB文件|*.rmvb|WMV文件|*.wmv|MKV文件|*.mkv|所有文件|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                grabber = new Capture(openFileDialog.FileName);
+                Application.Idle += new EventHandler(updateFrame);
+                //新版的3.x  記得預設為double 必須 使用  cast運算
+                //videoFps = (int)CvInvoke.cveVideoCaptureGet(grabber, CapProp.Fps);
+
+                //舊版的2.x以前 在 新版的已經不適用
+                //videoFps = (int)CvInvoke.cvGetCaptureProperty(grabber, Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FPS);
+            }
+        }
+
+        private void updateFrame(object sender, EventArgs e)
+        {
+            currentFrame = grabber.QueryFrame();
+            grabber.Stop();
+            if (currentFrame != null)
+            {
+                grabber.Stop();
+                inQueue.Enqueue(currentFrame);
+                label2.Text = inQueue.Count.ToString();
+                //label4.Text = videoFps.ToString();
+                label4.Text = CvInvoke.cveVideoCaptureGet(grabber, CapProp.Fps).ToString();
+                pictureBox1.Image = currentFrame.ToImage<Bgr, byte>().ToBitmap();
+                
+                /*
+                int i = 0;
+                //存影格用的   可以存下檢查  Queue 中  是否有影格
+                foreach (Mat img in inQueue)
+                {
+                    i++;
+                    try
+                    {
+                        img.Save(@"C:\frame\img" + i.ToString() + ".jpg");
+                        //為了避免出現 Additional information: 在 GDI+ 中發生泛型錯誤。 加上一組try... catch...
+                        //錯誤說明link : https://dotblogs.com.tw/atowngit/2010/01/13/13003
+                    }
+                    catch
+                    {
+
+                    }
+                }
+                */
+            }
+            if (isJump)
+            {
+                currentFrame = inQueue.ElementAt(frameNumber);
+                pictureBox1.Image = currentFrame.ToImage<Bgr, byte>().ToBitmap();
+            }
+        }
+
+        private void btnQueueClear_Click(object sender, EventArgs e)
+        {
+            inQueue.Clear();
+            label2.Text = inQueue.Count.ToString();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            Application.Idle += new EventHandler(updateFrame);
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            Application.Idle -= new EventHandler(updateFrame);
+        }
+
+        private void btnJump_Click(object sender, EventArgs e)
+        {
+            frameNumber = int.Parse(textBox1.Text);
+            //inQueue.ElementAt(frameNumber);
+            isJump = true;
+        }
+    }
+}
